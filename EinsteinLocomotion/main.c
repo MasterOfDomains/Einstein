c0c0647c9a1a2742b25f739bd3363723e78c4896
MasterOfDomains <jsrogers1@gmail.com>
2017-07-17 20:37:07 -0400

    Renames EinsteinMotors to EinsteinLocomotion

---
+++ EinsteinLocomotion/main.c
@@ -0,0 +1,18 @@
+/*
+ * EinsteinLocomotion.c
+ *
+ * Created: 7/17/2017 8:34:57 PM
+ * Author : Joe
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
